d418021fbf88e73f2ef95d9585fee915e2f85139
Brahms <brahmsisme@gmail.com>
2024-04-26 13:00:07 +0200

    Create Person Class

---
+++ src/main/java/dev/brahms/Person.java
@@ -0,0 +1,39 @@
+package dev.brahms;
+
+public class Person {
+
+    private String firstname;
+    private String lastname;
+    private char gender;
+
+    public Person(String firstname, String lastname, char gender) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.gender = gender;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+    
+}
